421de142917d408eee564bdba606c29834b12394
brycerobinsn <bryce.robinson@gmail.com>
2022-11-08 19:43:29 -0800

    add OAuth boilerplate

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+//google OAuth Login Route
+router.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    prompt: 'select_account',
+  })
+);
+
+//GoogleOAuth callback route
+router.get(
+  '/ouath2callback',passport.authenticate('google', {
+    successRedirect: '/calendar',
+    failureRedirect: '/auth/google'
+  })
+)
+
+// OAuth Logout Route
+router.get('/logout', function (req,res){
+  req.logout(function(err){
+    if (err){
+      return next(err);
+    }
+    res.redirect('/calendar');
+  });
 });
 
 module.exports = router;
